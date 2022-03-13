010153f98f9a917d67ed4a6a42602b33e7542b3e
Jack Kingham <wowknead@gmail.com>
2022-03-13 18:40:40 -0400

    Done most of task 8. This is a bad commit message because I'm apathetic and anxious from procrastinating on the tasks for multiple weeks.

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,33 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [inProgress, setInProgress] = useState<boolean>(false);
+    const [numAttempts, setNumAttempts] = useState<number>(4);
+
+    function startQuiz(): void {
+        setInProgress(true);
+        setNumAttempts(numAttempts - 1);
+    }
+
+    return (
+        <div>
+            <Button onClick={() => setInProgress(false)} disabled={!inProgress}>
+                Stop Quiz
+            </Button>
+
+            <Button
+                onClick={startQuiz}
+                disabled={inProgress || numAttempts === 0}
+            >
+                Start Quiz
+            </Button>
+            <Button
+                onClick={() => setNumAttempts(numAttempts + 1)}
+                disabled={inProgress}
+            >
+                Mulligan
+            </Button>
+            <div>{numAttempts}</div>
+        </div>
+    );
 }
--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 /**
  * Here is a helper function you *must* use to "roll" your die.
@@ -12,5 +12,38 @@ export function d6(): number {
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    const [leftDie, setLeftDie] = useState<number>(1);
+    const [rightDie, setRightDie] = useState<number>(2);
+
+    return (
+        <div>
+            <Container>
+                <Row>
+                    <Col>
+                        <Button onClick={() => setLeftDie(d6())}>
+                            Roll Left
+                        </Button>
+                    </Col>
+                    <Col>
+                        <Button onClick={() => setRightDie(d6())}>
+                            Roll Right
+                        </Button>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <span data-testid="left-die">Left Die: {leftDie} </span>
+                    </Col>
+                    <Col>
+                        <span data-testid="right-die">
+                            Right Die: {rightDie}
+                        </span>
+                    </Col>
+                </Row>
+            </Container>
+
+            {leftDie === rightDie && leftDie !== 1 && <div>You Win!</div>}
+            {leftDie === rightDie && leftDie === 1 && <div>You Lose!</div>}
+        </div>
+    );
 }
